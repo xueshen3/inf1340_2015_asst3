709d2f8f4f6a05627b4c0841478e2c8aff105c42
Bertha <bertha.chan@mail.utoronto.ca>
2015-12-15 12:26:02 -0500

    Updated test case

--- test_exercise2.py
+++ test_exercise2.py
@@ -11,17 +11,29 @@ import pytest
 import os
 from exercise2 import *
 
-DIR = "test_jsons/"
+DIR = os.getcwd()
+DIR += "/test_jsons/"
 os.chdir(DIR)
 
 
-# def test_returning():
-#     """
-#     Travellers are returning to KAN.
-#     """
-#     assert decide("test_returning_citizen.json", "countries.json") ==\
-#         ["Accept", "Accept", "Quarantine"]
+def test_returning():
+    """
+    Travellers are returning to KAN.
+    """
+    assert decide("test_returning_citizen.json", "countries.json") ==\
+        ["Accept", "Accept", "Quarantine"]
 
+
+def test_medical_advisory():
+    assert decide("test_medical_advisory.json", "countries.json") ==\
+       ["Quarantine", "Accept", "Quarantine"]
+
+# def test_case_sensitivity():
+#     """
+#     Travellers are returning to KAN. Country code and passport number correctness is checked.
+#     """
+#     assert decide("test_returning_citizen_2.json", "countries.json") ==\
+#         ["Accept", "Accept", "Quarantine"]
 
 
 def test_valid_date_format():
@@ -41,6 +53,7 @@ def test_passport_format():
     # Valid passport format
     assert valid_passport_format('JMZ0S-89IA9-OTCLY-MQILJ-P7CTY') == True
     assert valid_passport_format("12345-12345-67890-00000-00000") == True
+
     # Invalid passport format
     assert valid_passport_format("JMZ0S-89IA9-OTCLY-MQILJ-") == False
     assert valid_date_format('') == False
@@ -49,5 +62,8 @@ def test_passport_format():
 def test_visa_format():
     # Valid visa format
     assert valid_visa_format("CfR6x-XSMVA") == True
+
     # Invalid visa format
     assert valid_visa_format("xsmvA") == False
+    assert valid_visa_format("&*&%f-fid92") == False
+    assert valid_visa_format('') == False
