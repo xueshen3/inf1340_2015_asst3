97b16dbe9e1a0b7e664b57c7b838e174ff1e6ccf
Bertha <bertha.chan@mail.utoronto.ca>
2015-12-15 01:41:59 -0500

    Changed back valid_passport function

--- exercise2.py
+++ exercise2.py
@@ -51,60 +51,6 @@ def is_more_than_x_years_ago(x, date_string):
     return (date - x_years_ago).total_seconds() < 0
 
 
-def decide(input_file, countries_file):
-    """
-    Decides whether a traveller's entry into Kanadia should be accepted
-
-    :param input_file: The name of a JSON formatted file that contains
-        cases to decide
-    :param countries_file: The name of a JSON formatted file that contains
-        country data, such as whether an entry or transit visa is required,
-        and whether there is currently a medical advisory
-    :return: List of strings. Possible values of strings are:
-        "Accept", "Reject", and "Quarantine"
-    """
-    # open the json file to read and store date then close right away
-    with open(input_file) as json_file1:
-        input_data = json.load(json_file1)
-        json_file1.close()
-    with open(countries_file) as json_file2:
-        country_data = json.load(json_file2)
-        json_file2.close()
-    res_list = []
-    for i in input_data:
-        # create a temporary list to append temporary decision
-        tem_list = []
-        # check the traveller is coming from with a medical advisory
-        if i['from']['country'] in country_data:
-            if country_data[i['from']['country']]["medical_advisory"] is not '':
-                tem_list.append("Quarantine")
-        # check the traveller is through with a medical advisory
-        if i['home']['country'] in country_data:
-            if country_data[i['home']['country']]["medical_advisory"] is not '':
-                tem_list.append("Quarantine")
-        # check the information whether is complete
-        if ('' in i.values()) and ('' == i['from']['country']) and ('' == i['home']['country']):
-            tem_list.append("Rejected")
-        if valid_date_format(i["birth_date"]) is False:
-            tem_list.append("Rejected")
-        if valid_passport_format(i["passport"]) is False:
-            tem_list.append("Rejected")
-        # check whether is from KAN
-        if i['home']['country'].upper() == "KAN":
-            tem_list.append("Accepted")
-        # check if the reason for entry is to visit and requires valid visa
-        # A valid visa is one that is less than two years old.
-        if i['entry_reason'].lower() == 'visit' and country_data[i['from']['conutry']]["visitor_visa_required"] is '1':
-            if valid_visa_format(i["visa"]["code"]) and is_more_than_x_years_ago(2,i["visa"]["date"]):
-                tem_list.append("Accepted")
-            else:
-                tem_list.append("Rejected")
-        # call helper function base on priority to make the final decision
-        decide_helper(res_list,tem_list)
-
-    return res_list
-
-
 def decide_helper(res_list, tem_list):
     """
     Decides whether a traveller's entry into Kanadia
@@ -149,20 +95,19 @@ def valid_visa_format(visa_code):
 
 def valid_passport_format(passport_number):
     """
-    Checks whether a pasport number is five sets of five alpha-number characters separated by dashes
+    Checks whether a passport number is five sets of five alpha-number characters separated by dashes
     :param passport_number: alpha-numeric string
     :return: Boolean; True if the format is valid, False otherwise
     """
-    counter = 0
-    # Check each substring separated by "-" if its alphanumeric
-    for x in passport_number.split('-'):
-        x.strip()
-        if x.isalnum() and len(x) == 5:
-            # Increase counter if one set is valid
-            counter += 1
-    # If counter is 5 then it must be a valid format
-    if counter == 5:
-        return True
+    rex = re.match(r'^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)+(-[a-zA-Z0-9]+)+(-[a-zA-Z0-9]+)+(-[a-zA-Z0-9]+)$',passport_number)
+    # valid length must be 29
+    if len(passport_number) == 29:
+        # if rex is not None, means there find a match
+        if rex is not None:
+            return True
+    # it is None then return False
+        else:
+            return False
     else:
         return False
 
@@ -187,3 +132,33 @@ def valid_date_format(date_string):
         return True
     else:
         return False
+
+#####################
+# MAIN FUNCTION ##
+#####################
+
+
+def decide(input_file, countries_file):
+    """
+    Decides whether a traveller's entry into Kanadia should be accepted
+
+    :param input_file: The name of a JSON formatted file that contains
+        cases to decide
+    :param countries_file: The name of a JSON formatted file that contains
+        country data, such as whether an entry or transit visa is required,
+        and whether there is currently a medical advisory
+    :return: List of strings. Possible values of strings are:
+        "Accept", "Reject", and "Quarantine"
+    """
+    with open(input_file) as data_file:
+        cases = json.load(data_file)
+    with open(countries_file) as data_file:
+        countries = json.load(data_file)
+    decisions = ['Reject', 'Accept', 'Quarantine']
+    result = []
+    for case in cases:
+        print case
+
+    return result
+
+
