a3a51442d032507a12f150fd3dc6a8d2b4156af8
Bertha <bertha.chan@mail.utoronto.ca>
2015-12-15 00:40:24 -0500

    Added test cases

--- test_exercise2.py
+++ test_exercise2.py
@@ -9,20 +9,45 @@ __copyright__ = "2015 INF 1340 Assignment 3"
 # imports one per line
 import pytest
 import os
-from exercise2 import decide
+from exercise2 import *
 
 DIR = "test_jsons/"
 os.chdir(DIR)
 
 
-def test_returning():
-    """
-    Travellers are returning to KAN.
-    """
-    assert decide("test_returning_citizen.json", "countries.json") ==\
-        ["Accept", "Accept", "Quarantine"]
+# def test_returning():
+#     """
+#     Travellers are returning to KAN.
+#     """
+#     assert decide("test_returning_citizen.json", "countries.json") ==\
+#         ["Accept", "Accept", "Quarantine"]
 
 
-# def test_valid_visa():
-#     assert decide("test_valid_visa.json", "countries.json") ==\
-#         ["Accept"]
+
+def test_valid_date_format():
+    """
+    Checks date, must be in format yyyy-mm-dd & in numbers
+    """
+    assert (valid_date_format('')) == False
+    assert (valid_date_format('March 06,1999')) == False
+    assert (valid_date_format('2011.02.02')) == False
+    assert (valid_date_format('Mar.16, 2035')) == False
+    assert (valid_date_format('11-11-11')) == False
+    assert (valid_date_format('2016-January-16')) == False
+    assert valid_date_format('9768e-ab1de-8bc14-a3c4e-b12de') == False
+
+
+def test_passport_format():
+    # Valid passport format
+    assert valid_passport_format('JMZ0S-89IA9-OTCLY-MQILJ-P7CTY') == True
+    assert valid_passport_format("12345-12345-67890-00000-00000") == True
+    # Invalid passport format
+    assert valid_passport_format("JMZ0S-89IA9-OTCLY-MQILJ-") == False
+    assert valid_date_format('') == False
+
+
+def test_visa_format():
+    # Valid visa format
+    assert valid_visa_format("CfR6x-XSMVA") == True
+    # Invalid visa format
+    assert valid_visa_format("xsmvA") == False
